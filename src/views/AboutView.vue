be5e64c3971b7decc3cc7ec8ac56a26a6e08c01e
Rain Tüür <rain.tuur@gmail.com>
2026-04-06 15:54:37 +0300

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { PhDetective } from '@phosphor-icons/vue'
+</script>
+<template>
+  <div class="container text-center">
+    <h1>This is an about page</h1>
+    <span><PhDetective :size="52" color="#3fbefd" weight="duotone" /></span>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="container text-center">
+    <TheWelcome />
+  </div>
+</template>
