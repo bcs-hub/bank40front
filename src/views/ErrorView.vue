f69891a046338c69ac1cf432d777342da35760db
Marten <martenmagi1412@gmail.com>
2026-04-17 12:41:18 +0300

    feb-5 complete

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Midagi läks valesti :D</h1>
+        <img
+          src="https://media.tenor.com/N-WYlyfnjDsAAAAi/pepe-skill-issue.gif"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
