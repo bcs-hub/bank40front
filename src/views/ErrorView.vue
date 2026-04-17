e3cebe8cc93e31ba4a672e8163a6ad4efd12df99
luhtmakaili <kaili_l@hotmail.com>
2026-04-17 13:03:28 +0300

    Opetaja feb 5 (#16)
    * WIP
    
    * FEB-5 Complete
    
    ---------
    
    Co-authored-by: Rain Tüür <rain.tuur@gmail.com>

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Uups, midagi läks valesti :)</h1>
+        <img src="https://media.tenor.com/nANqORN7qhQAAAAM/explosion-explode.gif" class="img-fluid"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
