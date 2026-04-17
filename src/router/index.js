55b498d6638bbead193d6660c04962c871fff604
Rem <remtrofimov@gmail.com>
2026-04-17 12:37:36 +0300

    FEB-5 Complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import ErrorView from '@/views/ErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'homeRoute',
       component: HomeView,
-    }
+    },
+    {
+      path: '/error',
+      name: 'errorRoute',
+      component: ErrorView,
+    },
   ],
 })
 
