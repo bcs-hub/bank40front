9508239af5750dcc5ea321a4029a42f83b68d180
Rain Tüür <rain.tuur@gmail.com>
2026-04-17 15:19:53 +0300

    FEB-6 Complete

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Pangaautomaadid</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AtmsView',
+}
+</script>
