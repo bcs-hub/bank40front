4ee8e9b97be10b7c7390acd725d2b2c405366a5f
SANDER <sander.k6rts@gmail.com>
2026-04-17 14:23:49 +0300

    FEB-5 completed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="container text-center"></div>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Tere </h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'HomeView',
+}
 </script>
