60ae6a9ac8af5e52dd6ddbec54e088549a338e79
SANDER <sander.k6rts@gmail.com>
2026-04-17 15:19:52 +0300

    FEB-6 complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,13 @@
   <div class="container text-center">
     <div class="row">
       <div class="col">
-        <h1>Tere </h1>
+        <h1>Siit leiad kõik Eesti pangaautomaatide asukohad</h1>
+      </div>
+      <div class="col">
+        <img
+          src="https://www.zacekfoto.ee/wp-content/uploads/images/pilt1569.jpg"
+          class="img-fluid"
+        />
       </div>
     </div>
   </div>
