04a7efcfcab29daba5814a246e2d1603743aa0c3
margarittaerikson-ops <margaritta.erikson@gmail.com>
2026-04-17 13:09:14 +0300

    Margaritta feb 5 (#18)
    * Error 1
    
    * FEB-5 complete1

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Midagi läks valesti :)</h1>
+        <img
+          src="https://tenor.com/view/mmt-error-error-old-data-fatal-error-inscryption-gif-6023676896000678543"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
