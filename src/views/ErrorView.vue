ca55b84b11aec7f26498f50a2b94dbdead3f88a8
Hendrik Elmet <hendrikrasmus@hotmail.com>
2026-04-17 13:03:55 +0300

    FEB-5 complete (#2)

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Midagi läks valesti!!!!!:)</h1>
+        <img src="https://media.tenor.com/5nZkA47SzacAAAAC/bomb-joypixels.gif" class="img-fluid">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Error view'
+}
+</script>
