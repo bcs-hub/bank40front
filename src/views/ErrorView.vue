7027c749a0945940e47373379ed4acb717377508
rainer79-pixel <rainer79@gmail.com>
2026-04-17 12:37:37 +0300

    FEB-5 Complete

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <h1>Midagi läks valesti</h1>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVPUogCLXVNyHOHdtxmPKQtdIxpXhfdrkrnf7X8XAMdcAEQEEhdYFxm6pH4z4HmB-0GImtysaO4AWbKRhjsPdd-PeShhzR0w2rkJ8cTdww&s=10"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
