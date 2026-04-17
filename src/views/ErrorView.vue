70c28c5abac4fd5e1f42f8e2741c41daabab64c8
Madis <mivaste@gmail.com>
2026-04-17 12:39:23 +0300

    Added /error path with image

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">ErrorView</div>
+      <h1>Uups, midagi läks valesti :(</h1>
+      <img
+        src="https://cdn.dribbble.com/userupload/21875036/file/original-2c665754811e77db18418ccbce7cc9dd.gif"
+        class="img-fluid"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
