f464a71afcb8ee2d36ad68c81f7ed4c362a45ec8
Raudsepp <aadronraudsepp@gmail.com>
2026-04-17 12:39:05 +0300

    FEB-5 Complete

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Midagi läks pekki :)</h1>
+        <img
+          src="https://cliply.co/wp-content/uploads/2021/07/372107370_CROSS_MARK_400px.gif"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
