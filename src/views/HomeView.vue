923c7e9f2f61022fa3de5bba3753d3da5839ab39
Marten <martenmagi1412@gmail.com>
2026-04-17 15:29:13 +0300

    Lisasin ATM alused

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="container text-center"></div>
+  <div class="container text-center">
+    <div class="container text-center">
+      <div class="row">
+        <h1>Siit leiad kõik Eesti pangaautomaatide asukohad</h1>
+        <div class="col"></div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2hgoBtKxPpjSnH4opasMldukQJi1ZOlDRUw&s"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
