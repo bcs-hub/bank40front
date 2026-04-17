f52c85878045b95ae0def1177fb3372a6faa4d73
Even <evenmarten@gmail.com>
2026-04-17 12:40:15 +0300

    FEB-5 Complete

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Uups, midagi laks valesti :)</h1>
+        <img
+          src="https://media.tenor.com/kqXJzGr5cnIAAAAM/simpol-khaby-lame.gif"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
