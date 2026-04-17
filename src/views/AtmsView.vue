7b0e15823678b4a69f489e338fa2d41c7b5021aa
opilane <riho@powerful-marketers.com>
2026-04-17 15:21:15 +0300

    FEB-6 complete

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>pangaautomaadid</h1>
+      </div>
+      <div class="col">Column</div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AtmsView',
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="container text-center"></div>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Siit leiad kõik eesti pangaautomaatide asukohad</h1>
+      </div>
+      <div class="col">
+        <img
+          src="https://visitestonia.com/images/748170/panga-pank-ja-puhkekoht-015-visit-estonia.jpeg"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
