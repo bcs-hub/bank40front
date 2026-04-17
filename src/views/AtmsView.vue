6e4b70f9a635dfb67fb4648e60c1d0fe28e814a4
Raudsepp <aadronraudsepp@gmail.com>
2026-04-17 15:20:15 +0300

    FEB-6 Complete

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <h1>Pangaautomaadid</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AtmsView',
+}
+</script>
+<style scoped></style>
