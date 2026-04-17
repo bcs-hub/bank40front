8eff75d1e00e4493e542df473816a244c482c2ca
Rem <remtrofimov@gmail.com>
2026-04-17 15:19:54 +0300

    FEB-6 Complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="container text-center"></div>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Siit leiad kõik Eesti pangaautomaatide asukohad</h1>
+      </div>
+      <div class="col">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWkYgv22ad7FrGNpwstuid5S9cDaf_d68CLg&s"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
