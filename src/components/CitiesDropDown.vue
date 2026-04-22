35da25a39900e48fce79af37c2cf6a90e3c1dd57
SANDER <sander.k6rts@gmail.com>
2026-04-22 16:06:26 +0300

    FEB-9-WIP-2

---
+++ src/components/CitiesDropDown.vue
@@ -0,0 +1,23 @@
+<template>
+  <select
+    @change="$emit('event-new-city-sleceted', Number($event.target.value))"
+    :value="selectedCityId"
+    class="form-select"
+    aria-label="Vali linn"
+  >
+    <option selected :value="0">-- Kõik linnad --</option>
+    <option v-for="city in cities" :key="city.cityId" :value="city.cityId">
+      {{ city.cityName }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'CitiesDropDown',
+  props: {
+    cities: Object,
+    selectedCityId: Number,
+  },
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -7,12 +7,11 @@
     </div>
     <div class="row">
       <div class="col col-3">
-        <select class="form-select" aria-label="Default select example">
-          <option selected>-- Kõik linnad --</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
+        <CitiesDropDown
+          :cities="cities"
+          :selected-city-id="selectedCityID"
+          @event-new-city-selected="setSelectedCityId"
+        />
       </div>
     </div>
   </div>
@@ -21,26 +20,43 @@
 <script>
 import AuthService from '@/auth/AuthService.js'
 import CityService from '@/api-services/CityService.js'
+import NavigationService from '@/navigation/NavigationService.js'
+import CitiesDropDown from '@/components/CitiesDropDown.vue'
 
 export default {
   name: 'AtmsView',
+  components: { CitiesDropDown },
   data() {
     return {
       userId: AuthService.getLoggedInUserId(),
       roleName: AuthService.getLoggedInUserRoleName(),
+      selectedCityID: 0,
+      cities: [
+        {
+          cityId: 0,
+          cityName: '',
+        },
+      ],
     }
   },
   methods: {
     getCities() {
       CityService.sendGetCitiesRequest()
+        .then((response) => this.handleGetCitiesResponse(response))
+        .catch(() => NavigationService.navigateToErrorView())
+        .finally()
+    },
+    handleGetCitiesResponse(response) {
+      this.cities = response.data
+    },
+    setSelectedCityId(selectedCityId) {
+      this.selectedCityID = selectedCityId
     },
   },
-
   beforeMount() {
     this.getCities()
   },
 
-    //alert('userId: ' + this.userId + ' roleName: ' + this.roleName)
-  },
+  //alert('userId: ' + this.userId + ' roleName: ' + this.roleName)
 }
 </script>
