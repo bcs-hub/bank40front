8d306e8b7319377ee426de112cf86d7200ca519b
Hendzu <hendrikrasmus@hotmail.com>
2026-04-22 12:14:03 +0300

    FEB-9 WIP-1

---
+++ src/api-services/CityService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default {
+  sendGetCitiesRequest(){
+    return axios.get('/api/cities')
+  }
+
+
+}
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -5,11 +5,22 @@
         <h1>Pangaautomaadid</h1>
       </div>
     </div>
+    <div class="row">
+      <div class="col-4">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>-- Kõik Linnad --</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import AuthService from '@/auth/AuthService.js'
+import cityService from '@/api-services/CityService.js'
 
 export default {
   name: 'AtmsView',
@@ -19,8 +30,14 @@ export default {
       roleName: AuthService.getLoggedInUserRoleName(),
     }
   },
-  mounted() {
-    alert('userId: '+this.userId+' roleName:'+this.roleName)
+  methods: {
+    getCities() {
+      cityService.sendGetCitiesRequest()
+    },
+  },
+  beforeMount() {
+    // alert('userId: '+this.userId+' roleName:'+this.roleName)
+    this.getCities()
   },
 }
 </script>
