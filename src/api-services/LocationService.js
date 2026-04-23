6062089d19ea6a7dc14bcdf3bf263f0a11e3b414
Hendzu <hendrikrasmus@hotmail.com>
2026-04-23 12:09:19 +0300

    FEB-10 WIP-1

---
+++ src/api-services/LocationService.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export default {
+  sendGetAtmsLocationsRequest(cityId){
+    return axios.get('/api/atm/locations',{
+      headers:{Prefer:this.getPreferValue(cityId)},
+      params:{
+        cityId:cityId
+      }
+    })
+  },
+  getPreferValue(cityId){
+    switch (cityId) {
+      case 0:
+        return 'code=200, example=0'
+      case 1:
+        return 'code=404, example=1'
+      default:
+        return 'code=200, example=2'
+    }
+  }
+}
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -6,12 +6,35 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-4">
+      <div class="col-3">
         <CitiesDropdown
           :cities="cities"
           :selectedCityId="selectedCityId"
-          @event-selected-city-changed="selectedCityId=$event"
+          @event-selected-city-changed="setSelectedCityId"
         />
+      </div>
+      <div class="col-6">
+        <AlertError :error-message="errorMessage" />
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Linn</th>
+              <th scope="col">Asukoht</th>
+              <th scope="col">Teenused</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tartu</td>
+              <td>Tasku</td>
+              <td>
+                <div>sularaha välja</div>
+                <div>sularaha sisse</div>
+                <div>maksed</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -22,10 +45,12 @@ import AuthService from '@/auth/AuthService.js'
 import CityService from '@/api-services/CityService.js'
 import NavigationService from '@/navigation/NavigationService.js'
 import CitiesDropdown from '@/components/CitiesDropdown.vue'
+import LocationService from '@/api-services/LocationService.js'
+import AlertError from '@/components/AlertError.vue'
 
 export default {
   name: 'AtmsView',
-  components: { CitiesDropdown },
+  components: { CitiesDropdown, AlertError },
   data() {
     return {
       userId: AuthService.getLoggedInUserId(),
@@ -36,7 +61,24 @@ export default {
           cityName: '',
         },
       ],
-      selectedCityId:0,
+      locations: [
+        {
+          locationId: 0,
+          locationName: '',
+          cityName: '',
+          transactionTypes: [
+            {
+              transactionTypeName: '',
+            },
+          ],
+        },
+      ],
+      selectedCityId: 0,
+      errorResponse: {
+        message: '',
+        errorCode: 0,
+      },
+      errorMessage: '',
     }
   },
   methods: {
@@ -46,13 +88,37 @@ export default {
         .catch(() => NavigationService.navigateToErrorView())
         .finally()
     },
+    getLocations() {
+      this.errorMessage=''
+      LocationService.sendGetAtmsLocationsRequest(this.selectedCityId)
+        .then((response) => this.handleGetAtmsLocationsResponse(response.data))
+        .catch((error) => this.handleGetLocationsError(error.response))
+        .finally()
+    },
     handleGetCitiesResponse(response) {
       this.cities = response.data
+    },
+    setSelectedCityId(event) {
+      this.selectedCityId = Number(event)
+      this.getLocations()
+    },
+    handleGetAtmsLocationsResponse(locations) {
+      this.locations = locations
+    },
+
+    handleGetLocationsError(error) {
+      const statusNumber = error.status
+      this.errorResponse = error.data
+      if (statusNumber === 404 && this.errorResponse.errorCode === 222) {
+        this.errorMessage = this.errorResponse.message
+      } else {
+      }
     },
   },
   beforeMount() {
     // alert('userId: '+this.userId+' roleName:'+this.roleName)
     this.getCities()
+    this.getLocations()
   },
 }
 </script>
