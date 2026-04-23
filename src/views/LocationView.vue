b88c954fc290b70eba5b1624f0ae693bd6dd5a3c
Rain Tüür <rain.tuur@gmail.com>
2026-04-23 16:08:22 +0300

    FEB-11 WIP 3

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -5,8 +5,8 @@
         <h1>Lisa asukoht</h1>
       </div>
     </div>
-    <div class="row">
-      <div class="col">
+    <div class="row justify-content-center">
+      <div class="col col-2">
         <CitiesDropdown
           :cities="cities"
           :selected-city-id="selectedCityId"
@@ -14,11 +14,35 @@
           :first-option-is-disabled="true"
         />
       </div>
-      <div class="col">
-        <h4>vorm</h4>
+      <div class="col col-2">
+        <div>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="inputLocationName" placeholder="Asukoht" />
+            <label for="inputLocationName">Asukoht</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              type="number"
+              min="1"
+              class="form-control"
+              id="inputNumberOfAtms"
+              placeholder="Automaatide arv"
+            />
+            <label for="inputNumberOfAtms">Automaatide arv</label>
+          </div>
+          <div class="form-check mb-3">
+            <input class="form-check-input" type="checkbox" id="transactionTypeId-1" />
+            <label class="form-check-label" for="checkDefault"> Sularaha sisse </label>
+          </div>
+        </div>
       </div>
       <div class="col">
         <h4>pilt</h4>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col col-3">
+        <ImageInput />
       </div>
     </div>
   </div>
@@ -28,10 +52,11 @@
 import CitiesDropdown from '@/components/CitiesDropdown.vue'
 import CityService from '@/api-services/CityService.js'
 import NavigationService from '@/navigation/NavigationService.js'
+import ImageInput from '@/components/ImageInput.vue'
 
 export default {
   name: 'LocationView',
-  components: { CitiesDropdown },
+  components: { ImageInput, CitiesDropdown },
   data() {
     return {
       selectedCityId: 0,
