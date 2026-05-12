a527bf94ae70333d43542a5f506c9ec50f0b673a
Rain Tüür <rain.tuur@gmail.com>
2026-05-12 14:34:45 +0300

    FEB-12 WIP 3

---
+++ src/components/location/LocationInfoModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <BaseModal :isOpen="isOpen" @event-modal-closed="$emit('event-modal-closed')">
+    <template #title>{{ location.locationName }}</template>
+    <template #body>
+      <div class="d-flex gap-3">
+        <div>
+          <p><strong>Automaatide arv:</strong> {{ location.numberOfAtms }}</p>
+          <div
+            v-for="transactionType in location.transactionTypes"
+            :key="transactionType.transactionTypeName"
+          >
+            {{ transactionType.transactionTypeName }}
+          </div>
+        </div>
+        <AtmImage :imageData="location.imageData" />
+      </div>
+    </template>
+  </BaseModal>
+</template>
+
+<script>
+import BaseModal from '@/components/BaseModal.vue'
+import AtmImage from '@/components/AtmImage.vue'
+
+export default {
+  name: 'LocationInfoModal',
+  components: { BaseModal, AtmImage },
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+    location: Object,
+  },
+  emits: ['event-modal-closed'],
+}
+</script>
